dec586b1009d01397715b731c267b4efc8d60add
ItsXirano <xiranowmc78@gmail.com>
2016-10-26 19:55:53 +0200

    Finalise Installer.php
    + Add Server Ram Config

--- src/pocketmine/wizard/Installer.php
+++ src/pocketmine/wizard/Installer.php
@@ -1,20 +1,22 @@
 <?php
 
 /*
- *
- *  ____            _        _   __  __ _                  __  __ ____  
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
+ *    ########:'##:::::::'##:::'##:'##:::::'##:'####:'##::: ##::'######:::
+ *    ##.....:: ##:::::::. ##:'##:: ##:'##: ##:. ##:: ###:: ##:'##... ##::
+ *    ##::::::: ##::::::::. ####::: ##: ##: ##:: ##:: ####: ##: ##:::..:::
+ *    ######::: ##:::::::::. ##:::: ##: ##: ##:: ##:: ## ## ##: ##::'####:
+ *    ##...:::: ##:::::::::: ##:::: ##: ##: ##:: ##:: ##. ####: ##::: ##::
+ *    ##::::::: ##:::::::::: ##:::: ##: ##: ##:: ##:: ##:. ###: ##::: ##::
+ *    ########: ########:::: ##::::. ###. ###::'####: ##::. ##:. ######:::
+ *    ........::........:::::..::::::...::...:::....::..::::..:::......::::
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
+ * @author H4PM Team
+ * @link http://www.github.net/H4PM
  * 
  *
 */
@@ -48,14 +50,14 @@ class Installer{
      ........::........:::::..::::::...::...:::....::..::::..:::......::::! \n";// Fully Updated ! ?
 		echo " >>Alpha Stage  \n";//
 		echo " >>--------------------------------\n";//
-   		echo " >>Pulsed By : @SuperMaXAleX_  @ReskillDEV @Misteboss_mcpe @Pab45O \n"; //TODO: Add @xiranoMC If you acccept this update !
+   		echo " >>Pulsed By : @SuperMaXAleX_  @ReskillDEV @Misteboss_mcpe @Pab45O @xiranoMc \n"; 
 		echo " >>MCPE Version : 0.16.0.5 Alpha Beta: $version   \n";// Yeay
 		echo " >>Please select a language:\n";//TODO: Add A Auth For MCPC // Soon ?
 		foreach(InstallerLang::$languages as $short => $native){
 	               echo " $native => $short\n";
 		}
 		do{
-			echo "[?] Language (en): ";
+			echo "Default Language is English <en>: ";
 			$lang = strtolower($this->getInput("en"));
 			if(!isset(InstallerLang::$languages[$lang])){
 				echo "[!] Couldn't find the language\n";
@@ -65,14 +67,14 @@ class Installer{
 		$this->lang = new InstallerLang($lang);
 
 
-		echo "[*] " . $this->lang->language_has_been_selected . "\n";
+		echo ">> " . $this->lang->language_has_been_selected . "\n";
 
 		if(!$this->showLicense()){
 			\pocketmine\kill(getmypid());
 			exit(-1);
 		}
 
-		echo "[?] " . $this->lang->skip_installer . " (y/N): ";
+		echo ">> " . $this->lang->skip_installer . " (y/N): ";
 		if(strtolower($this->getInput()) === "y"){
 			return;
 		}
@@ -96,9 +98,9 @@ class Installer{
   (at your option) any later version.
 
 LICENSE;
-		echo "\n[?] " . $this->lang->accept_license . " (y/N): ";
+		echo "\n>> " . $this->lang->accept_license . " (y/N): ";
 		if(strtolower($this->getInput("n")) != "y"){
-			echo "[!] " . $this->lang->you_have_to_accept_the_license . "\n";
+			echo ">> " . $this->lang->you_have_to_accept_the_license . "\n";
 			sleep(5);
 
 			return false;
@@ -108,9 +110,9 @@ LICENSE;
 	}
 
 	private function welcome(){
-		echo "[*] " . $this->lang->setting_up_server_now . "\n";
-		echo "[*] " . $this->lang->default_values_info . "\n";
-		echo "[*] " . $this->lang->server_properties . "\n";
+		echo ">> " . $this->lang->setting_up_server_now . "\n";
+		echo ">> " . $this->lang->default_values_info . "\n";
+		echo ">> " . $this->lang->server_properties . "\n";
 
 	}
 
@@ -127,9 +129,9 @@ LICENSE;
 			}
 		}while($port <= 0 or $port > 65535);
 		$config->set("server-port", $port);
-		/*echo "[*] " . $this->lang->ram_warning . "\n";
+		echo "[*] " . $this->lang->ram_warning . "\n";
 		echo "[?] " . $this->lang->server_ram . " (" . self::DEFAULT_MEMORY . "): ";
-		$config->set("memory-limit", ((int) $this->getInput(self::DEFAULT_MEMORY)) . "M");*/
+		$config->set("memory-limit", ((int) $this->getInput(self::DEFAULT_MEMORY)) . "M");
 		echo "[*] " . $this->lang->gamemode_info . "\n";
 		do{
 			echo "[?] " . $this->lang->default_gamemode . ": (" . self::DEFAULT_GAMEMODE . "): ";
